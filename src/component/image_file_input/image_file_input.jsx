6539ecff1555a8a3b295fb5541c6092247f5acd1
ClarXo <imp_rove@naver.com>
2021-10-10 08:15:00 +0900

    add card and edit from component

---
+++ src/component/image_file_input/image_file_input.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../../common/colors.css';
+
+const ImageFileInput = (props) => (
+      <button>Image</button>
+  );
+
+export default ImageFileInput;
