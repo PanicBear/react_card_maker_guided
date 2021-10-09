0f6d9163cbad75e19fcb9a2dcb2766c060cf0bdc
ClarXo <imp_rove@naver.com>
2021-10-09 14:23:56 +0900

    add login and logout function
    - pass user object using callback to auth.onAuthStateChange

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./app.module.css";
+import styles from "./app.module.css";
 import Login from "./component/login/login";
-import styles from './app.module.css';
+import Maker from "./component/maker/maker";
 
 function App({ authService }) {
   return (
     <div className={styles.app}>
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route exact path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/login/login.jsx
+++ src/component/login/login.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import Footer from '../footer/footer';
-import Header from '../header/header';
-import styles from './login.module.css';
+import React, { useEffect } from "react";
+import { useHistory } from "react-router";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+import styles from "./login.module.css";
 
 const Login = ({ authService }) => {
+  const history = useHistory();
+  const goToMaker = (userId) => {
+    history.push({
+      pathname: "/maker",
+      state: { id: userId },
+    });
+  };
   const onLogin = (event) => {
     authService
       .login(event.currentTarget.textContent)
-      .then(console.log);
-  }
+      .then((data) => goToMaker(data.user.uid));
+  };
+
+  useEffect(()=>{
+    authService.onAuthChange(user=>{
+      user && goToMaker(user.uid);
+    })
+  },[])
+
   return (
     <section className={styles.login}>
       <Header />
@@ -16,16 +31,20 @@ const Login = ({ authService }) => {
         <h1>Login</h1>
         <ul className={styles.list}>
           <li className={styles.item}>
-            <button className={styles.button} onClick={onLogin}>Google</button>
+            <button className={styles.button} onClick={onLogin}>
+              Google
+            </button>
           </li>
           <li>
-            <button className={styles.button} onClick={onLogin}>Github</button>
+            <button className={styles.button} onClick={onLogin}>
+              Github
+            </button>
           </li>
         </ul>
       </section>
       <Footer />
     </section>
-  )
+  );
 };
 
 export default Login;
